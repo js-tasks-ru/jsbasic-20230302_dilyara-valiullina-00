869b7771f8d4f307e73f1af1185835e2b3efe782
Diliara Valiullina <dilyara.valiullina.00@bk.ru>
2023-04-07 20:28:59 +0300

    6-module-1-task-UserTable

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -1,18 +1,51 @@
-/**
- * Компонент, который реализует таблицу
- * с возможностью удаления строк
- *
- * Пример одного элемента, описывающего строку таблицы
- *
- *      {
- *          name: 'Ilia',
- *          age: 25,
- *          salary: '1000',
- *          city: 'Petrozavodsk'
- *      }
- *
- */
+function createElement(html) {
+  const wrapper = document.createElement('div');
+  wrapper.innerHTML = html;
+  return wrapper.firstElementChild;
+}
+
 export default class UserTable {
+  #rows = '';
   constructor(rows) {
+    this.#rows =rows;
+    this.elem = this.#render();
+  }
+  #template() {
+    return `
+    <div class="example">
+      <table>
+        <thead>
+          <tr>
+            <th>Имя</th>
+            <th>Возраст</th>
+            <th>Зарплата</th>
+            <th>Город</th>
+            <th></th>
+          </tr>
+      </thead>
+      <tbody>
+          ${this.#rows.map(row => `
+            <tr>
+              <td>${row.name}</td>
+              <td>${row.age}</td>
+              <td>${row.salary}</td>
+              <td>${row.city}</td>
+              <td><button>X</button></td>
+            </tr>
+          `).join('')}
+      </tbody>
+      </table>
+    </div>`;
+  }
+  #onMenuClick = (event) => {
+    const button = event.target;
+    const row = button.closest('tr');
+    row.remove();
+  };
+  #render() {
+    const elem = createElement(this.#template());
+    elem.addEventListener('click', this.#onMenuClick);
+    return elem;
   }
 }
+
